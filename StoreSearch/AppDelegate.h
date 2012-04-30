78672d946b02f9f0bbf0deb8ced3f76bea0f4b75
Ashok Gelal <ashokgelal@gmail.com>
2012-04-30 00:52:37 -0600

    Initial Commit

---
+++ StoreSearch/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  StoreSearch
+//
+//  Created by Ashok Gelal on 4/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ StoreSearch/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  StoreSearch
+//
+//  Created by Ashok Gelal on 4/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
